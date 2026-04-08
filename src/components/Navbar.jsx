be7d0df944ddd83fc14fcf88f8fb826f6d77247c
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-04-08 22:10:38 +0400

    I'm starting to rebuild the Birbank.az website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full bg-#fff ">
+        <div className="flex justify-between gap-4 w-full">
+          <div className="flex items-center align-center">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar

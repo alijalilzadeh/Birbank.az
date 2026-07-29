63169481435fd18db14d775d57810c702526ba3a
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-07-29 23:33:04 +0400

    Responsive codes added

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -29,29 +29,29 @@ const Credit = () => {
     }
   ]
   return (
-    <>
+    <div className='w-full min-h-screen overflow-x-hidden'>
       <Navbar />
       <SecondaryNavbar />
       <>
-        <div className="flex flex-col items-center justify-center w-full selection:bg-[#B3D4FC] mb-20">
-          <div className="flex flex-col w-[75%]">
-            <div className="flex justify-between p-12 w-full bg-[linear-gradient(265deg,_#fff1d3_8.07%,_#f3f4f6_70.76%)] rounded-[20px] relative overflow-hidden mb-20">
-              <div className="flex justify-between flex-col w-[50%] gap-10 ">
-                <div className="flex flex-col gap-4">
-                  <h2 className='text-[40px] font-bold text-[#25282b] leading-10'>İstəyinizə uyğun nağd pul krediti onlayn əldə edin!</h2>
-                  <p className='text-[16px] font-normal text-[#25282b] leading-normal'>Kapital Bankın təklif etdiyi kredit çeşidləri ilə ehtiyaclarınızı təxirə salmadan qarşılayın.</p>
+        <div className="flex flex-col items-center justify-center  selection:bg-[#B3D4FC] mb-20 ">
+          <div className="flex flex-col w-[95%] sm:w-[65%] md:w-full lg:w-[75%]">
+            <div className="flex flex-col-reverse p-4  sm:flex-col-reverse sm:items-center sm:justify-center md:flex-col-reverse lg:flex-row justify-between sm:p-4  lg:p-12 w-full bg-[linear-gradient(265deg,_#fff1d3_8.07%,_#f3f4f6_70.76%)] rounded-[20px] relative overflow-hidden mb-20">
+              <div className="flex justify-between flex-col w-full sm:w-full lg:w-[50%] gap-10 ">
+                <div className="flex flex-col gap-4 mt-2 sm:mt-2 lg:mt-0">
+                  <h2 className='w-full text-center text-[36px]  sm:text-center sm:w-full md:w-full md:text-[36px] lg:text-[40px] lg:text-start font-bold text-[#25282b] leading-10'>İstəyinizə uyğun nağd pul krediti onlayn əldə edin!</h2>
+                  <p className='text-[16px] font-normal text-center sm:text-center text-[#25282b] leading-normal lg:text-start '>Kapital Bankın təklif etdiyi kredit çeşidləri ilə ehtiyaclarınızı təxirə salmadan qarşılayın.</p>
                 </div>
-                <Link to='/kredit-sifarisi' className='inline-flex w-fit py-2 px-8 text-[14px] font-semibold bg-[#ec3342] rounded-4xl text-white' reloadDocument>Sifariş</Link>
+                <Link to='/kredit-sifarisi' className='inline-flex w-fit py-2 px-8 text-[14px] self-center sm:self-center lg:self-start font-semibold bg-[#ec3342] rounded-4xl text-white' reloadDocument>Sifariş</Link>
               </div>
-              <div className="flex items-center justify-center w-[50%]  ">
-                <img src={ProductInner} className='w-full object-cover flex ' alt="BirPay" />
+              <div className="flex items-center justify-center w-full sm:w-full md:w-full lg:w-[50%]  ">
+                <img src={ProductInner} className='w-115 sm:w-126 md:w-184 lg:w-155 object-cover flex ' alt="BirPay" />
               </div>
             </div>
 
-            <div className="grid grid-cols-6 items-center justify-center w-full gap-6 h-full ">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 items-center justify-center w-full gap-6 h-full ">
               {
                 creditData.map((item, id) => (
-                  <div className="col-span-2 row-span-2 flex flex-col gap-3 rounded-[20px] h-full overflow-hidden bg-[linear-gradient(to_bottom,_#fff1d3_0%,_#f3f4f6_100%)]">
+                  <div className=" flex flex-col gap-3 rounded-[20px] h-full overflow-hidden bg-[linear-gradient(to_bottom,_#fff1d3_0%,_#f3f4f6_100%)]">
                     <img src={item.imgUrl} className='object-cover items-center justify-center' />
                     <div className="flex flex-col gap-6 p-6">
                       <div className="flex flex-col justify-between gap-2">
@@ -65,13 +65,12 @@ const Credit = () => {
                 ))
 
               }
-
             </div>
           </div>
         </div>
       </>
       <Footer />
-    </>
+    </div>
   )
 }
 
